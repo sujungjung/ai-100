aa696c16ba007360927918c354738d4d6e304309
sujungjung <jsj3435323@gmail.com>
2024-08-07 13:27:44 +0900

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title('동물 이미지 찾아주기')
-
-title = st.text_input('영어로 동물 이름을 입력하세요')
-if st.button('찾아보기'):
-    url = 'http://edu.spartacodingclub.kr/random/?'+ title
-    st.image(url)
